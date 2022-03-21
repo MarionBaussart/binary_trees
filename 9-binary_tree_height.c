bc626c56ca7b906bc96a8d328d4313eb29c09bdb
MarionBaussart <marion.baussart@gmail.com>
2022-03-21 17:02:13 +0000

    T12

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
 /**
  * binary_tree_height - function that measures the height of a binary tree
@@ -19,7 +18,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		height_left = binary_tree_height(tree->left) + 1;
 		height_right = binary_tree_height(tree->right) + 1;
 	}
-	
+
 	if (height_left > height_right)
 		return (height_left);
 	else
