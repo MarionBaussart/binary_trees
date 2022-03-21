80e968e8105921cb2abe1822c383c6554797912a
Secatricia <3560@holbertonschool.com>
2022-03-21 12:06:01 +0000

    Add change position

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,15 +12,17 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
+
 
 	if (parent->right == NULL)
 	{
