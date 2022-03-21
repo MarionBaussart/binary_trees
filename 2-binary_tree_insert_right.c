1ea193c2e3eaebf9aa4b90c24a9a202ddbc8f166
MarionBaussart <marion.baussart@gmail.com>
2022-03-21 12:11:32 +0000

    merge

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,11 +12,10 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
 	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
