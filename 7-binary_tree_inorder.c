0b9b1b2baaafceec140c79d94e18936c5d7add86
Secatricia <3560@holbertonschool.com>
2022-03-21 13:42:59 +0000

    Add ex 7 structure

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: Pointer to the node to check
+ * Return: 1 if node is a root, otherwise 0
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		
+	}
+
+}
