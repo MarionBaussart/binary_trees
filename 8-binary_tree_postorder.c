c5fd7dc1f9b0c907016aa71dbd608eb4dc990c6b
MarionBaussart <marion.baussart@gmail.com>
2022-03-21 13:58:06 +0000

    T8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree
- * using pre-order traversal
+ * binary_tree_postorder - function that goes through a binary tree
+ * using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function that take an int as parameter
  * Return: no return
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree)
+	if (tree && func)
 	{
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
