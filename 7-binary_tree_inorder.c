d826cfa5c1ae8b37779183a3fc6bf57f368d551e
Secatricia <3560@holbertonschool.com>
2022-03-21 14:00:29 +0000

    Add ex 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - checks if a given node is a root
- * @node: Pointer to the node to check
- * Return: 1 if node is a root, otherwise 0
+ * binary_tree_inorder - checks if a given node is a root
+ * @tree: Pointer to the root node of the tree to traverse
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
