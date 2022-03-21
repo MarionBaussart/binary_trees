d897fe60cf6884f72cfd1a41be3bb8a005ec0e40
MarionBaussart <marion.baussart@gmail.com>
2022-03-21 14:01:43 +0000

    T6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,8 +11,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree)
 	{
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
-		func(tree->n);
 	}
 }
