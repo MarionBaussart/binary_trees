2e7e5a228ef024ac02c9939b7a0abd074c9b30d0
Secatricia <3560@holbertonschool.com>
2022-03-22 14:57:10 +0000

    Add ex 102

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+
+}
