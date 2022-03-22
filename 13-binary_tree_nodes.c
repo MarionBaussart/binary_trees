765bf5e595a559014da2bca5a93a80d940f58a38
MarionBaussart <marion.baussart@gmail.com>
2022-03-22 12:24:55 +0000

    end of mandatory

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,7 +8,7 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nb_nodes = 0;
-	
+
 	if (tree == NULL)
 		return (0);
 
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -33,7 +33,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	if (nb_leaves % 2 == 0)
 		return (1);
 	else
