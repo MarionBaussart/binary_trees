70027fbfe8690835ab84403479592b9363cd6a0e
MarionBaussart <marion.baussart@gmail.com>
2022-03-22 14:04:18 +0000

    correct T15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,4 +1,24 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ * Return: number of nodes with child, 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nb_nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree && (tree->left != NULL || tree->right != NULL))
+	{
+		nb_nodes = binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right) + 1;
+	}
+
+	return (nb_nodes);
+}
 
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
@@ -28,13 +48,15 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	size_t nb_leaves = 0;
+	size_t nb_internal_nodes = 0;
 
 	nb_leaves = binary_tree_leaves(tree);
+	nb_internal_nodes = binary_tree_nodes(tree);
 
 	if (tree == NULL)
 		return (0);
 
-	if (nb_leaves % 2 == 0)
+	if (nb_leaves == nb_internal_nodes + 1)
 		return (1);
 	else
 		return (0);
