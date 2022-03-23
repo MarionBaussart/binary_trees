08dde51c8d2d8e8f328f25948a23c8ce7f5057e7
Secatricia <3560@holbertonschool.com>
2022-03-23 13:20:50 +0000

    Add ex 103 and 104

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -3,33 +3,22 @@
 /**
  * binary_tree_rotate_left - performs a left-rotation on a binary tree
  * @tree: Pointer to the root node of the tree to check
- * Return: 0 or 1
+ * Return: tree or NULL
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-<<<<<<< HEAD
-	binary_tree_t *tmp;
-
-	if (tree)
+	if (tree && tree->right)
 	{
+		tree->parent = tree->right;
 		if (tree->right->left)
-		{
-			tmp = (binary_tree_t *)tree->right;
-
-			tree->right = tmp->left;
-			tree->right->parent = tree;
-		
-			tmp->left = tree;
-			tmp->parent = tree->parent;
-			tree->parent = tmp;
-			
-
+			tree->right = tree->parent->left;
+		else
+			tree->right = NULL;
+		tree->parent->left = tree;
+		tree->parent->parent = NULL;
+		return (tree->parent);
 	}
-	return (tree);
-=======
-	Pivot = tree→CO
-	tree→CO = tree→right
-	Pivot→CR = tree
-	tree = Pivot
->>>>>>> 78b26a67d8c289686366c31e154bb5599135b2e1
+	else
+		return(NULL);
+	
 }
