de5eb06fa26a21f5171cd5b44abd434d9358331d
MarionBaussart <marion.baussart@gmail.com>
2022-03-24 13:14:56 +0000

    T111

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
 /**
  * bst_insert - function that inserts a value in a Binary Search Tree
@@ -9,80 +8,42 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new = (*tree);
+	bst_t *node;
 
-	printf("new create new->n: %d\n", new->n);
-
-	if (new)
+	if (*tree == NULL)
 	{
-		printf("new new->n: %d\n", new->n);
-		if (value == (new->n))
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+
+	node = (*tree);
+
+	while (node)
+	{
+		if (value == (node->n))
+			return (NULL);
+
+		if (value > (node->n))
 		{
-			free (new);
-			return (*tree);
-		}
-		if (value > (new->n))
-		{
-			if (new->right)
-			{
-				printf("go right new->n: %d\n", new->n);
-				new = new->right;
-				bst_insert(&new, value);
-			}
+			if (node->right)
+				node = node->right;
 			else
-				return (binary_tree_insert_right(new, value));
-		}
-		else if (value < new->n)
-		{
-			if (new->left)
 			{
-				printf("go left new->n: %d\n", new->n);
-				new = new->left;
-				bst_insert(&new, value);
+				node->right = binary_tree_node(node, value);
+				return (node->right);
 			}
+		}
+		else if (value < node->n)
+		{
+			if (node->left)
+				node = node->left;
 			else
-				return (binary_tree_insert_left(new, value));
+			{
+				node->left = binary_tree_node(node, value);
+				return (node->left);
+			}
 		}
 	}
-	return (binary_tree_node(new, value));
-}
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new;
-
-	if (parent == NULL)
-		return (NULL);
-
-	new = binary_tree_node(parent, value);
-
-	if (parent->left == NULL)
-		parent->left = new;
-	else
-	{
-		parent->left->parent = new;
-		new->left = parent->left;
-		parent->left = new;
-	}
-	return (new);
-}
-
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new;
-
-	if (parent == NULL)
-		return (NULL);
-
-	new = binary_tree_node(parent, value);
-
-	if (parent->right == NULL)
-		parent->right = new;
-	else
-	{
-		parent->right->parent = new;
-		new->right = parent->right;
-		parent->right = new;
-	}
-	return (new);
+	return (NULL);
 }
